18a6f7fd003c8c7cb0f9e22b08fa19c919db9566
Antonella D Atri <antonella.datri@alu.unq.edu.ar>
2020-10-04 16:14:18 -0300

    el personaje se puede mover y saltar al mismo tiempo

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,5 +1,5 @@
 extends KinematicBody2D
-export (int) var speed = 200
+
 var velocity = Vector2()
 
 export var run_speed = 100
@@ -11,19 +11,23 @@ func get_input():
 	var right = Input.is_action_pressed('ui_right')
 	var left = Input.is_action_pressed('ui_left')
 	var jump = Input.is_action_just_pressed('ui_up')
+	
 	if right:
 		velocity.x += run_speed
-		$AnimatedSprite.play("walk")
+		if is_on_floor():
+			$AnimatedSprite.play("walk")
 		$AnimatedSprite.set_flip_h(false)
-	elif left:
+	if left:
 		velocity.x -= run_speed
-		$AnimatedSprite.play("walk")
+		if is_on_floor():
+			$AnimatedSprite.play("walk")
 		$AnimatedSprite.set_flip_h(true)
-	elif is_on_floor() and jump:
+	if is_on_floor() and jump:
 		velocity.y = jump_speed
 		$AnimatedSprite.play("jump")
-	elif is_on_floor():
+	if is_on_floor() and not left and not right and not jump:
 		$AnimatedSprite.play("stop")
+
 
 func _physics_process(delta):
 	velocity.y += gravity * delta
