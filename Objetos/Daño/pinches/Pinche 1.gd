51b2b93ad2da8eebb48e71b3ca4c64ced1dd9c28
Antonella D Atri <antonella.datri@alu.unq.edu.ar>
2020-11-06 16:42:00 -0300

    nivel 2 cuasi terminado

---
+++ Objetos/Daño/pinches/Pinche 1.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+signal hit
+signal hitClone(clone)
+
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		emit_signal("hit")
+	if body.is_in_group("clon"):
+		emit_signal("hitClone",body)
+		
