3d02efa173f4ab5a56c5575a9ad7baa6e86b0fbd
Antonella D Atri <antonella.datri@alu.unq.edu.ar>
2020-11-12 01:35:21 -0300

    bases de un nuevo nivel

--- nivel/Nivel 4/Nivel4.gd
+++ nivel/Nivel 4/Nivel4.gd
@@ -2,15 +2,16 @@ extends Node2D
 
 onready var player = $Player
 export var cantDeClones = 3
-onready var plataformaH = $Plataformas/PlataformasMoviles/PlatBot/Plataforma
-onready var plataformaV1 = $Plataformas/PlataformasMoviles/PlatBot2/Plataforma
-onready var plataformaV2 = $Plataformas/PlataformasMoviles/PlatBot3/Plataforma
+onready var plataformaH = $Plataformas/PlataformaH/Plataforma
+onready var plataformaV1 = $Plataformas/Plataforma1/Plataforma
+onready var plataformaV2 = $Plataformas/Plataforma2/Plataforma
+onready var plataformaV3 = $Plataformas/Plataforma3/Plataforma
 
 func _ready():
 	player.setCantLimiteClones(cantDeClones)
-	plataformaH.start(0, 0, 0, 140)
-	plataformaV1.start(95, 0, 0, 0)
-	plataformaV2.start(95, 0, 0, 0)
+	plataformaH.start(0, 0, 195, 0)
+	plataformaV1.start(176, 0, 0, 0)
+	plataformaV2.start(0, 210, 0, 0)
 
 # warning-ignore:unused_argument
 func _physics_process(delta):
@@ -19,11 +20,11 @@ func _physics_process(delta):
 	elif player.new_clon != null:
 		$Camera2D.position =  player.new_clon.position
 
-func _on_Boton_apretado():
-	plataformaH.derecha(0.5)
-
-func _on_Boton_suelto():
+func _on_BotonH_apretado():
 	plataformaH.izquierda(0.5)
+
+func _on_BotonH_suelto():
+	plataformaH.derecha(0.5)
 
 func _on_game_over():
 	$TimerDead.start()
@@ -45,10 +46,10 @@ func _on_BotonV1_suelto():
 	plataformaV1.bajar(0.5)
 
 func _on_BotonV2_apretado():
-	plataformaV2.subir(0.5)
+	plataformaV2.bajar(0.5)
 
 func _on_BotonV2_suelto():
-	plataformaV2.bajar(0.5)
+	plataformaV2.subir(0.5)
 
 func _win_game(body):
 	if body.is_in_group("player"):
