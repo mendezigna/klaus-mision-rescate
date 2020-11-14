73d777c4f9155433fa30ab4b06dfec3950295ee8
Aldana Castro <aldanacastro1999@gmail.com>
2020-11-13 21:37:18 -0300

    Arreglo nivel 2 Error

--- Player/Player.gd
+++ Player/Player.gd
@@ -39,10 +39,71 @@ func setCantLimiteClones(numero):
 	cantLimite = numero
 	setLabelText()
 
-##
 func setLabelText():
 	$CanvasLayer/Label.text = "Restantes: " + str(cantLimite - clones.size())
 
+func _physics_process(delta):
+	velocity.x = 0
+	var snap = 3
+	siEstaActivo()
+	if estaVivo:
+		if velocity.y != 0:
+			snap = Vector2(0,0)
+			caer()
+		elif velocity.x == 0 and sprite.animation != "summon" and sprite.animation != "land":
+			parar()
+		clonar()
+		manejarPlayer()
+		reiniciarClones()
+	# warning-ignore:return_value_discarded
+	move_and_slide_with_snap(velocity, Vector2.DOWN * snap, Vector2(0, -1), false)
+	siEstaEnElPiso(delta)
+	interfaceTimer.text = "Revivir en: " + str((ceil(get_tree().get_nodes_in_group("time")[0].get_time_left())))
+
+func siEstaActivo():
+	if activo:
+		get_input()
+
+func siEstaEnElPiso(delta):
+	if is_on_floor():
+		velocity.y = 0
+		if jumping and estaVivo:
+			sprite.play("land")
+			sprite.offset.x = 0
+			sprite.offset.y = 0
+			jumping = false
+			$CPUParticles2D.emitting = true 
+	else:
+		velocity.y += gravity * delta
+
+func clonar():
+	if Input.is_action_just_pressed("clonar") and clones.size() < cantLimite and is_on_floor():
+			agregarClon()
+			sprite.play("summon")
+
+func manejarPlayer():
+	if Input.is_action_just_pressed("interactuar") and clones.size() > 0:
+			activar()
+			if new_clon != null:
+				new_clon.desactivar()
+
+func reiniciarClones():
+	if Input.is_action_just_pressed("reiniciar_clones") and clones.size() > 0:
+			remover_clones()
+
+func parar():
+	sprite.play("stop")
+	sprite.offset.x = 0
+	sprite.offset.y = 0
+
+func caer():
+	if velocity.y > 0 :
+		sprite.play("falling")
+		if velocity.x > 0 or !sprite.flip_h:
+			sprite.offset.x = -2
+		elif velocity.x < 0 or sprite.flip_h:
+			sprite.offset.x = 2
+		sprite.offset.y = 3.5
 
 func get_input():
 	if (estaVivo):
@@ -74,26 +135,6 @@ func get_input():
 			sprite.offset.x = 0
 			sprite.offset.y = 0
 
-func desactivar():
-	activo = false
-	if sprite.animation != "land" and sprite.animation != "summon":
-		sprite.play("stop")
-		sprite.offset.x = 0
-		sprite.offset.y = 0
-
-func activar():
-	activo = true
-
-func remove_clon(clon):
-	if clon == new_clon:
-		new_clon = null
-	colors.append(clon.modulate.to_html())
-	clones.erase(clon)
-	orbes[clones.size()].show()
-	clon.morir()
-	setLabelText()
-	activar()
-
 func agregarClon():
 	if clones.size() > 0 && new_clon != null:
 		new_clon.desactivar()
@@ -107,73 +148,30 @@ func agregarClon():
 	new_clon.activar()
 	setLabelText()
 
+func desactivar():
+	activo = false
+	if sprite.animation != "land" and sprite.animation != "summon":
+		sprite.play("stop")
+		sprite.offset.x = 0
+		sprite.offset.y = 0
+
 func remover_clones():
 	for clon in get_tree().get_nodes_in_group("clon"):
 		clon.desactivar()
 		remove_clon(clon)
 
-func _physics_process(delta):
-	velocity.x = 0
-	var snap = 3
-	siEstaActivo()
-	if estaVivo:
-		if velocity.y != 0:
-			snap = Vector2(0,0)
-			if velocity.y > 0 :
-				sprite.play("falling")
-				if velocity.x > 0 or !sprite.flip_h:
-					sprite.offset.x = -2
-				elif velocity.x < 0 or sprite.flip_h:
-					sprite.offset.x = 2
-				sprite.offset.y = 3.5
-			
-		elif velocity.x == 0 and sprite.animation != "summon" and sprite.animation != "land":
-			sprite.play("stop")
-			sprite.offset.x = 0
-			sprite.offset.y = 0
-		clonar()
-		manejarPlayer()
-		reiniciarClones()
+func remove_clon(clon):
+	if clon == new_clon:
+		new_clon = null
+	colors.append(clon.modulate.to_html())
+	clones.erase(clon)
+	orbes[clones.size()].show()
+	clon.morir()
+	setLabelText()
+	activar()
 
-# warning-ignore:return_value_discarded
-	move_and_slide_with_snap(velocity, Vector2.DOWN * snap, Vector2(0, -1), false)
-
-	siEstaEnElPiso(delta)
-
-		
-##	if (estaVivo):
-
-	interfaceTimer.text = "Revivir en: " + str((ceil(get_tree().get_nodes_in_group("time")[0].get_time_left())))
-
-func siEstaActivo():
-	if activo:
-		get_input()
-
-func siEstaEnElPiso(delta):
-	if is_on_floor():
-		velocity.y = 0
-		if jumping and estaVivo:
-			sprite.play("land")
-			sprite.offset.x = 0
-			sprite.offset.y = 0
-			jumping = false
-			$CPUParticles2D.emitting = true 
-	else:
-		velocity.y += gravity * delta
-func clonar():
-	if Input.is_action_just_pressed("clonar") and clones.size() < cantLimite and is_on_floor():
-			agregarClon()
-			sprite.play("summon")
-
-func manejarPlayer():
-	if Input.is_action_just_pressed("interactuar") and clones.size() > 0:
-			activar()
-			if new_clon != null:
-				new_clon.desactivar()
-
-func reiniciarClones():
-	if Input.is_action_just_pressed("reiniciar_clones") and clones.size() > 0:
-			remover_clones()
+func activar():
+	activo = true
 
 func morir():
 	if estaVivo:
@@ -194,7 +192,6 @@ func win():
 	
 func pararMusica():
 	$AudioFondo.stop()
-
 
 func _on_AnimatedSprite_animation_finished():
 	if sprite.animation != "dead" and sprite.animation != "walk" and sprite.animation != "jump" and sprite.animation != "falling":
