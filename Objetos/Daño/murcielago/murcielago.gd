eeaac03fde0228523b410ade2fea33559513a2b2
uliseslopez98 <uliseslopez1112@gmail.com>
2020-11-15 23:34:03 -0300

    codigo araña y murcielago
    las agregue al segundo nivel
    les faltarian los assets

---
+++ Objetos/Daño/murcielago/murcielago.gd
@@ -0,0 +1,20 @@
+extends "res://Objetos/Daño/Daño.gd"
+
+var vel = 1
+export var gravedadY = 0
+export var gravedadX = -1.1
+
+func _process(delta):
+		position.y -= gravedadY * vel * delta
+		position.x -= gravedadX * vel * delta
+		vel = min(vel+3,80)
+
+func _on_body_entered(body):
+	if(not body.is_in_group("player")):
+		gravedadY = gravedadY * -1
+		gravedadX = gravedadX * -1
+		$Sprite.flip_h = not $Sprite.flip_h
+	else:
+		gravedadY = 0
+		gravedadX = 0
+		
